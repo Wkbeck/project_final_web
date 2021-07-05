0cfc65c0abda97bf1de5e360de6e0351d9ef33f0
Beck <esso.space@hotmail.com>
2021-07-05 08:34:05 +0700

    curent 7/5/2021

--- connect_db.js
+++ connect_db.js
@@ -1,31 +1,49 @@
-let i = 0 
+let i = 0
 
 //show data
-async function show_data(){
-    
-    db.collection("users").get().then(user =>{
-        user.docs.forEach(doc => {
-                    document.querySelector("#name_th"+doc.id.toString()).innerText = "Name: "+doc.data().name;
-                    document.querySelector("#surname_th"+doc.id.toString()).innerText = "Surname: "+doc.data().surname;
-                    document.querySelector("#status"+doc.id.toString()).innerText = "Status: "+doc.data().status;
-                    
-                    
-        });
-    });
-    
-}
+ /* async function show_data() {
+
+     db.collection("users").get().then(user => {
+         user.docs.forEach(doc => {
+             document.querySelector("#name_th" + doc.id.toString()).innerText = "Name: " + doc.data().name;
+             document.querySelector("#surname_th" + doc.id.toString()).innerText = "Surname: " + doc.data().surname;
+             document.querySelector("#status" + doc.id.toString()).innerText = "Status: " + doc.data().status;
+             if(doc.data().status=='ว่าง') {
+                 document.querySelector("#status" + doc.id.toString()).style.borderRadius = "8px";
+                 document.querySelector("#status" + doc.id.toString()).style.border = "solid 2px black";
+                 document.querySelector("#status" + doc.id.toString()).style.backgroundColor = "lightgreen";
+                 document.querySelector("#status" + doc.id.toString()).style.width = "350px";
+                 document.querySelector("#status" + doc.id.toString()).style.margin = "auto";
+             }else{
+                 document.querySelector("#status" + doc.id.toString()).style.borderRadius = "8px";
+                 document.querySelector("#status" + doc.id.toString()).style.border = "solid 2px black";
+                 document.querySelector("#status" + doc.id.toString()).style.backgroundColor = "#FF3300";
+                 document.querySelector("#status" + doc.id.toString()).style.width = "350px";
+                 document.querySelector("#status" + doc.id.toString()).style.margin = "auto";
+             }
+         });
+     });
+ } */
 //show realtime data
-db.collection("users").onSnapshot(snapshot =>{ 
-    db.collection("users").get().then(user =>{
+ db.collection("users").onSnapshot(snapshot => {
+    db.collection("users").get().then(user => {
         user.docs.forEach(doc => {
-            document.querySelector("#name_th"+doc.id.toString()).innerText = "Name: "+doc.data().name;
-            document.querySelector("#surname_th"+doc.id.toString()).innerText = "Surname: "+doc.data().surname;
-            document.querySelector("#status"+doc.id.toString()).innerText = "Status: "+doc.data().status;   
+            document.querySelector("#name_th" + doc.id.toString()).innerText = "Name: " + doc.data().name;
+            document.querySelector("#surname_th" + doc.id.toString()).innerText = "Surname: " + doc.data().surname;
+            document.querySelector("#status" + doc.id.toString()).innerText = "Status: " + doc.data().status;
+            if(doc.data().status=='ว่าง') {
+                document.querySelector("#status" + doc.id.toString()).style.borderRadius = "8px";
+                document.querySelector("#status" + doc.id.toString()).style.border = "solid 2px black";
+                document.querySelector("#status" + doc.id.toString()).style.backgroundColor = "lightgreen";
+                document.querySelector("#status" + doc.id.toString()).style.width = "350px";
+                document.querySelector("#status" + doc.id.toString()).style.margin = "auto";
+            }else{
+                document.querySelector("#status" + doc.id.toString()).style.borderRadius = "8px";
+                document.querySelector("#status" + doc.id.toString()).style.border = "solid 2px black";
+                document.querySelector("#status" + doc.id.toString()).style.backgroundColor = "#FF3300";
+                document.querySelector("#status" + doc.id.toString()).style.width = "400px";
+                document.querySelector("#status" + doc.id.toString()).style.margin = "auto";
+            }
         });
-        
-       
     });
-})
-
-
-show_data();
+}) 
--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+var t = 0 ,a = 0 ;
 $(function() {
   
     var count = $('.slide').length,
-        i = 1,
+        i=1,
         j=0,
         ids=1,
         t=true,
@@ -12,7 +13,8 @@ $(function() {
         $('.active').removeClass('active').next('.slide').addClass('active');
         ids++;
         i++;
-        console.log(ids);
+        
+       
       }
     }
     function prevSlide() {
@@ -20,7 +22,8 @@ $(function() {
         $('.active').removeClass('active').prev('.slide').addClass('active');
         ids--;
         i--;
-        console.log(ids);
+      
+        
       }
     }
     function togglePopup(){
@@ -28,70 +31,124 @@ $(function() {
         j=1;
         document.getElementById("popup-1").classList.toggle("active");
         if(ids==1){
-           var firebaseRef = firebase.database().ref('state_free');
-           firebaseRef.update({1 : 1});
+           var firebaseRef = firebase.database().ref('user/0001');
+           firebaseRef.update({noti : 1});
         }else if(ids==2){
-          var firebaseRef = firebase.database().ref('state_free');
-           firebaseRef.update({2 : 1});
+          var firebaseRef = firebase.database().ref('user/0002');
+           firebaseRef.update({noti : 1});
            
         }else if(ids==3){
-          var firebaseRef = firebase.database().ref('state_free');
-           firebaseRef.update({3 : 1});
+          var firebaseRef = firebase.database().ref('user/0003');
+           firebaseRef.update({noti : 1});
         
-        }else {
-          var firebaseRef = firebase.database().ref('state_free');
-           firebaseRef.update({4 : 1});
+        }else if(ids==4){
+          var firebaseRef = firebase.database().ref('user/0004');
+           firebaseRef.update({noti : 1});
+        }else if(ids==5){
+          var firebaseRef = firebase.database().ref('user/0005');
+          firebaseRef.update({noti : 1});
+        }else{
+          var firebaseRef = firebase.database().ref('user/0006');
+          firebaseRef.update({noti : 1});
         }
       }
+    
     }  
     function exit(){
       if(j==1){
         j=0;
         document.getElementById("popup-1").classList.toggle("active");
       }
+     
       
-    }      
+    }    
     window.onkeyup = function(e) {
       if (e.keyCode == 37) {
         nextSlide();
+        a=1;
       }
       if (e.keyCode == 39) {
         prevSlide();
+        a=1;
+        
       }
       if (e.keyCode == 32){
         togglePopup();
+        a=1;
       }
-      if (e.keyCode == 38){
+      if (e.keyCode == 38){ 
         exit();
+        a=1;
       }
-      
     }
-  
+ 
+
   });
+ 
   async function check_status(){
-    var firebaseRef = firebase.database().ref('state');
-    firebaseRef.once('value').then(function(dataSnapshot){
-      if(dataSnapshot.val()['1']=='1'){
+    var firebaseRef = firebase.database().ref('user/0001');
+    var firebaseRef2 = firebase.database().ref('user/0002');
+    var firebaseRef3 = firebase.database().ref('user/0003');
+    var firebaseRef4 = firebase.database().ref('user/0004');
+    var firebaseRef5 = firebase.database().ref('user/0005');
+    var firebaseRef6 = firebase.database().ref('user/0006');   
+    if(a==1){
+        t=0;
+        a=0;
+      }
+      t++;
+      if(t>45){
+        go_back();
+        t=0;
+      }
+    firebaseRef.once('value').then(function(dataSnapshot1){
+      if(dataSnapshot1.val()['status']=='1'){
        db.collection("users").doc('1').update({status: 'ว่าง'});
-      }else if(dataSnapshot.val()['1']=='0'){
+      }else if(dataSnapshot1.val()['status']=='0'){
            db.collection("users").doc('1').update({status: 'ไม่ว่าง'});
       }
-      if(dataSnapshot.val()['2']=='1'){
-        db.collection("users").doc('2').update({status: 'ว่าง'});
-      }else if(dataSnapshot.val()['2']=='0'){
-          db.collection("users").doc('2').update({status: 'ไม่ว่าง'});
+    })
+    firebaseRef2.once('value').then(function(dataSnapshot2){
+      if(dataSnapshot2.val()['status']=='1'){
+       db.collection("users").doc('2').update({status: 'ว่าง'});
+      }else if(dataSnapshot2.val()['status']=='0'){
+           db.collection("users").doc('2').update({status: 'ไม่ว่าง'}); 
       }
-       if(dataSnapshot.val()['3']=='1'){
-        db.collection("users").doc('3').update({status: 'ว่าง'});
-       }else if(dataSnapshot.val()['3']=='0'){
-          db.collection("users").doc('3').update({status: 'ไม่ว่าง'});
-      }  
-       if(dataSnapshot.val()['4']=='1'){
-        db.collection("users").doc('4').update({status: 'ว่าง'});
-       }else if(dataSnapshot.val()['4']=='0'){
-          db.collection("users").doc('4').update({status: 'ไม่ว่าง'});
+    })
+    firebaseRef3.once('value').then(function(dataSnapshot3){
+      if(dataSnapshot3.val()['status']=='1'){
+       db.collection("users").doc('3').update({status: 'ว่าง'});
+      }else if(dataSnapshot3.val()['status']=='0'){
+           db.collection("users").doc('3').update({status: 'ไม่ว่าง'});
+      }
+    })
+    firebaseRef4.once('value').then(function(dataSnapshot4){
+      if(dataSnapshot4.val()['status']=='1'){
+       db.collection("users").doc('4').update({status: 'ว่าง'});
+      
+      }else if(dataSnapshot4.val()['status']=='0'){
+           db.collection("users").doc('4').update({status: 'ไม่ว่าง'});  
+      }
+    })
+    firebaseRef5.once('value').then(function(dataSnapshot5){
+      if(dataSnapshot5.val()['status']=='1'){
+       db.collection("users").doc('5').update({status: 'ว่าง'});
+      }else if(dataSnapshot5.val()['status']=='0'){
+           db.collection("users").doc('5').update({status: 'ไม่ว่าง'});
+      }
+    })
+    firebaseRef6.once('value').then(function(dataSnapshot6){
+      if(dataSnapshot6.val()['status']=='1'){
+       db.collection("users").doc('6').update({status: 'ว่าง'});
+      }else if(dataSnapshot6.val()['status']=='0'){
+           db.collection("users").doc('6').update({status: 'ไม่ว่าง'});
       }
     })
   }
+  function go_back(){
+    window.location.href = "index.html"
+    t=0;
+  }
   check_status();
-  setInterval(check_status,1000);
+  setInterval(check_status,1000);  
+ 
